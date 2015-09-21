c11b17dea50b49ca2de26346c5ed06988fa7446d
Wendy Reyes <wendyrepez@gmail.com>
2015-09-20 21:02:24 -0400

    add login html file

---
+++ www/js/controllers/LoginController.js
@@ -0,0 +1,11 @@
+angular.module('cvonlineapp').controller('LoginController',
+  ['$scope','User','$state','ErrorHandler','$ionicHistory',
+  function($scope,User,$state,ErrorHandler,$ionicHistory){
+
+    console.log('login');
+/*  $scope.$on('$ionicView.enter', function(){
+    $scope.data = {};
+    $scope.btnDisabled = false;
+    $ionicHistory.clearCache();
+  })*/
+  }]);
