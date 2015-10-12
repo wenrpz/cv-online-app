c8976ba3a1dda756921b8ec37084b796e0c19147
Wendy Reyes <wendyrepez@gmail.com>
2015-10-12 00:10:05 -0400

    add put, delete and post method

--- www/js/api.js
+++ www/js/api.js
@@ -1,2 +1,42 @@
-angular.module('cvonlineapp').factory('API',function($resource){
+angular.module('cvonlineapp').factory('Api',function($http, $q, Constants){
+  return {
+    post: function(endpoint, params) {
+      var deferred = $q.defer();
+      $http({
+        url: Constants.API_URL + endpoint,
+        method: "POST",
+        data: params
+      })
+      .then(deferred.resolve, deferred.reject);
+      return deferred.promise;
+    },
+    put: function(endpoint, params) {
+      console.log($http.defaults.headers.common);
+      var deferred = $q.defer();
+      $http({
+        url: Constants.API_URL + endpoint,
+        method: "PUT",
+        data: params
+      })
+      .then(deferred.resolve, deferred.reject);
+      return deferred.promise;
+    },
+    delete: function(endpoint, params){
+      console.log($http.defaults.headers);
+     var deferred = $q.defer();
+       $http({
+        url: Constants.API_URL + endpoint,
+        method: "DELETE",
+        data: params
+      })
+      .then(deferred.resolve, deferred.reject);
+      return deferred.promise;
+    },
+    setDefaultHeader: function(key, value){
+      $http.defaults.headers.common[key] = value;
+    },
+    removeDefaultHeader: function(key){
+      delete $http.defaults.headers.common[key];
+    }
+  };
 });
