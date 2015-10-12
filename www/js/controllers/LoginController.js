3348823a976222ccd658a3163b7e026fa42b4466
Wendy Reyes <wendyrepez@gmail.com>
2015-10-12 00:13:03 -0400

    add login and profile calls

--- www/js/controllers/LoginController.js
+++ www/js/controllers/LoginController.js
@@ -1,14 +1,23 @@
-angular.module('cvonlineapp').controller('LoginController',['$scope','$state','$openFB', function($scope, $state, $openFB){
+angular.module('cvonlineapp').controller('LoginController', 
+  ['$scope','$state','ngFB', 'User', function($scope, $state, ngFB, User){
 
-  $scope.login = function(){
-     $openFB.login({scope: 'email'}).then(
-        function (token) {
-            if (token) {
-                console.log('Facebook login succeeded');
-                $state.go('app.cv');
-            } else {
-                alert('Facebook login failed');
-            }
-        });
-  }  
-}]);
+    $scope.login = function(){
+      ngFB.login({scope: 'email'}).then(function (response) {
+        if (response.status == 'connected') {
+          User.login(response.authResponse.accessToken, function(response){
+            console.log('CONTR SUCCESS', response);
+            $state.go('app.cv');
+          }, function(response){
+            console.log('CONTR ERROR', response);
+          });
+        } else {
+          alert('Facebook login failed');
+        }
+      });
+    }
+
+    if (User.isLoggedIn()){
+      $state.go('app.profile');
+    }
+  }]
+);
--- www/js/controllers/ProfileController.js
+++ www/js/controllers/ProfileController.js
@@ -1,8 +1,42 @@
-angular.module('cvonlineapp').controller('ProfileController',function($scope){
-  $scope.profile = {
-    name: "Fulanito Perez",
-    email: 'fulanito@gmail.com',
-    tel:'809-567-7643',
-    cel:'849-455-4666'
-  };
+angular.module('cvonlineapp').controller('ProfileController',function($scope, $state, ngFB, User){
+  $scope.profile = User.getProfile();
+
+  $scope.getPicture = function() {
+    ngFB.api({
+      path: '/me',
+      params: {fields: 'id,name,email,picture'}
+    }).then(
+      function (user) {
+        $scope.profile.facebook = user;
+      },
+      function (error) {
+          alert('Facebook error: ' + error.error_description);
+      }
+    );
+  }
+
+  $scope.logout = function() {
+    ngFB.logout().then(function() {
+      User.logout();
+      $state.go('login');
+    });
+  }
+
+  $scope.removeAccount =function() {
+    if (confirm('Desea eliminar la cuenta?')) {
+      User.deleteUser(function() {
+        console.log('User deleted');
+      });
+    }
+  }
+
+  $scope.save = function() {
+    User.update($scope.user, function(response) {
+      console.log(response);
+    },function(error) {
+      console.log(error);
+    });
+  }
+
+  $scope.getPicture();
 })
